a0e7588d5d399fccc09ab6e93d67875af20f8eaa
imoempire <mrimoempire@gmail.com>
2021-08-31 14:26:21 +0000

    edit edit case

--- src/Reducer/AccountsReducer.js
+++ src/Reducer/AccountsReducer.js
@@ -26,9 +26,25 @@ const AccountsReducer = ( state = initialState, action ) => {
               return {...state, accounts: SavedAccounts  }
         
         case EDIT_ACCOUNT:
-            return {...state, accounts: state.accounts.map((account) =>
-                account.id === action.payload.id ? action.payload : account
-              )}
+                    const savedAccounts = state.accounts.map((account)=>{
+                        if (account.id === action.id ) {
+                            return {
+                                id: account.id,
+                                account_name: action.account.account_name,
+                                account_number: action.account.account_number,
+                                account_type: action.account.account_type,
+                                bank_name: action.account.bank_name,
+                                bank_branch: action.account.bank_branch,
+                              };
+                        }
+                        return account;
+                    });
+                    return { ...state, accounts: savedAccounts };
+
+
+            // return {...state, accounts: state.accounts.map((account) =>
+            //     account.id === action.payload.id ? action.payload : account
+            //   )}
 
         default:
             return state;
