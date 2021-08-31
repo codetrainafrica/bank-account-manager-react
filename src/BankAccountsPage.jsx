57a879866ee06cf11df8078316c27b1d8bac85ef
Bill Zack <bzak954@gmail.com>
2021-08-31 15:05:05 +0000

    add state from store

--- src/BankAccountsPage.jsx
+++ src/BankAccountsPage.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import BankAccounts from './components/BankAccounts';
+import {connect} from 'react-redux';
+import { deleteAccount } from './Actions/accountAction';
 
 
-const BankAccountsPage = () => {
+const BankAccountsPage = ({state, deleteAccount}) => {
+  
     return (
         <div>
-            <BankAccounts />
+            <BankAccounts accounts={state.accounts} deleteAccount={deleteAccount}/>
         </div>
     );
 }
 
-export default BankAccountsPage;
+const mapStateToProps = (state) => { return {state:state} }
+
+const mapDispatchToProps = {deleteAccount}
+
+export default connect(mapStateToProps,mapDispatchToProps)(BankAccountsPage);
