dbdeca35126586505e6b4a9946cf19e29044b457
FREEDOM <freewhynane62@gmail.com>
2021-08-31 19:06:30 +0000

    remove editAccount argument

--- src/actions/accountActions.js
+++ src/actions/accountActions.js
@@ -1,14 +1,23 @@
 export const addAccount = (account) => {
+    account.id = Math.random().toString();
     return {
         type: "ADD_ACCOUNT",
         payload: account
     }
 }
 
-export const editAccount = (updateAccount) => {
+export const editAccount = (id, account) => {
     return {
         type: "EDIT_ACCOUNT",
-        payload: updateAccount
+        id,
+        account
+    }
+};
+
+export const deleteAccount = (account_id) => {
+    return {
+        type: "DELETE_ACCOUNT",
+        account_id
     }
 }
 
